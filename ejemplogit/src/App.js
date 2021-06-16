7af66a9b857c47083a25cf5fd4a9406327807a2d
Ana <anags.clm@gmail.com>
2021-06-16 19:11:22 +0200

    Pagina web terminada

---
+++ ejemplogit/src/App.js
@@ -0,0 +1,25 @@
+import logo from './whale.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          ¡Nos gustan las ballenas!
+        </p>
+        <a
+          className="App-link"
+          href="http://emailchk.com/whale/ballena.html"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Sorpresa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
